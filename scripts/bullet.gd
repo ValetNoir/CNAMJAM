d6fee436905db22afc0f7878e8620554cf5bcf16
ValetNoir <valetnoir.gaming@gmail.com>
2025-09-03 16:21:19 +0200

    bullet collision detection

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,5 +1,29 @@
 extends Node3D
 
-@export var element : String = "default"
+@onready var rigid_body_3d: RigidBody3D = $"."
 
-@onready var rigid_body_3d: RigidBody3D = $RigidBody3D
+const PLANT_AREA: PackedScene = preload("res://scenes/plant_area.tscn")
+
+# baies, fleurs, lianes, etc...
+var element: String = "default"
+var direction: Vector3
+
+@export var speed: float = 10.0
+
+func _on_body_entered(body: Node) -> void:
+    
+    var is_ground: bool = body.get_collision_layer_value(1)
+    
+    if element == "default":
+        # PAS DE ZONE, DEGATS SIMPLE SUR MOB
+        pass
+    else:
+        # SPAWN ZONE, DEGATS (+ EFFETS, juste zone ?) SUR MOB
+        spawn_zone()
+    
+    queue_free()
+
+func spawn_zone() -> void:
+    var plant_area = PLANT_AREA.instantiate()
+    plant_area.element = element
+    # add_child(plant_area)
---
+++ scripts/plant_area.gd
@@ -0,0 +1,6 @@
+extends Area3D
+
+@onready var plant_area: Area3D = $"."
+
+# baies, fleurs, lianes, etc...
+var element: String = "default"
